77bdf13362c0dfc393fa855fd6233df21acc649b
Jason Qu <fishinsea2014@gmail.com>
2017-11-27 12:58:42 +1300

    Finish change quesiton API.

--- xiaohu/app/Question.php
+++ xiaohu/app/Question.php
@@ -35,6 +35,36 @@ class Question extends Model
     /*Change a question
     */
     public function change(){
-        return 1;
+        //If not login, tips login
+        if (!user_ins()->is_log_in()){
+            return ['status'=>0,'msg'=>'please login. '];
+        }
+
+        //If no id, tips id.
+        if(!rq('id'))
+            return ['status'=>0,'msg'=>'id is required. '];
+
+        //Get module of the current id.
+        $question=$this->find(rq('id'));
+
+        //If no questions ,tips.
+        if(!$question)
+            return ['status'=>0,'msg'=>'question not exists.'];
+
+        //
+        if ($question->user_id !== session('user_id')){
+            return ['status'=>0,'msg'=>'permission denied'];
+        }
+
+        if(rq('title'))
+            $question->title=rq('title');
+
+        if(rq('desc'))
+            $question->desc=rq('desc');
+
+
+        return $question->save() ?
+            ['status'=>1]:
+            ['status'=>0,'msg'=>'db update failed'];
     }
 }
