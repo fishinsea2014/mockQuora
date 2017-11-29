56321ae1e20c19a3fb7a42647f3734992dd37381
Jason Qu <fishinsea2014@gmail.com>
2017-11-29 22:53:09 +1300

    Start front end programming.

---
+++ xiaohu/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: QSG
+ * Date: 29/11/2017
+ * Time: 10:52 PM
+ */
--- xiaohu/resources/views/welcome.blade.php
+++ xiaohu/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel hello wrold
                 </div>
 
                 <div class="links">
